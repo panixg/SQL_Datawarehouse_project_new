7527a019e91e7abe9c7bf65d28b249792985a326
Fede <leon.pualp@gmail>
2025-03-29 19:31:30 -0300

    Add advanced customer analysis SQL scripts and create a new customer report

--- scripts/EDA/Advanced EDA.sql
+++ scripts/EDA/Advanced EDA.sql
@@ -81,3 +81,81 @@ CASE WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORD
 END AS sales_performance
 FROM yearly_product_sales
 ORDER BY product_name
+
+--Part to Whole (Proportion Analysis)
+--Which category contributes the most to the total sales
+WITH category_sales AS(
+SELECT
+category,
+SUM(sales_amount) as total_sales
+FROM gold.fact_sales f
+    LEFT JOIN gold.dim_product p
+    ON f.product_key = p.product_key
+GROUP BY category)
+
+SELECT
+category,
+total_sales,
+SUM (total_sales) OVER () AS total_sales_all,
+CONCAT(ROUND ((CAST(total_sales AS FLOAT) / SUM (total_sales) OVER ()) * 100, 2), '%') AS porcentage_of_total_sales
+FROM category_sales
+ORDER BY total_sales DESC;
+
+--Data Segmentation
+--Segment products into cost ranges and count how many products fall into each range
+WITH product_segments AS(
+SELECT
+product_key,
+product_name,
+cost,
+CASE WHEN cost < 100 THEN 'Below 100 Cost'
+    WHEN cost >= 100 AND cost < 500 THEN '100-500 Cost'
+    WHEN cost >= 500 AND cost < 1000 THEN '500-1000 Cost'
+    ELSE 'Above 1000 Cost'
+END AS cost_range    
+FROM gold.dim_product)
+
+SELECT
+cost_range,
+COUNT(product_key) as total_products
+FROM product_segments
+GROUP BY cost_range
+ORDER BY total_products DESC;
+
+/* Group custumers into three segments based on their spending behavior:
+- VIP: Customers with at least 12 months of history and spending more than $5000
+- Regular: Customers with at least 12 months of history and spending  $5000 or less
+- New: Customers with less than 12 months of history 
+Find the total number of customers in each segment */
+
+WITH customer_segments AS(
+SELECT
+c.customer_key,
+c.first_name,
+c.last_name,
+SUM(f.sales_amount) as total_spending,
+MIN(f.order_date) AS first_order_date,
+MAX(f.order_date) AS last_order_date,
+DATEDIFF (month, MIN(f.order_date), MAX(f.order_date)) AS history_months
+FROM gold.fact_sales f
+    LEFT JOIN gold.dim_customer c
+    ON f.customer_key = c.customer_key
+GROUP BY c.customer_key , c.first_name, c.last_name
+)
+
+SELECT 
+customer_segment,
+COUNT(customer_key) as total_customers
+FROM (
+SELECT
+customer_key,
+first_name,
+last_name,
+total_spending,
+CASE WHEN history_months >= 12 AND total_spending > 5000 THEN 'VIP'
+    WHEN history_months >= 12 AND total_spending <= 5000 THEN 'Regular'
+    ELSE 'New'
+END AS customer_segment
+FROM customer_segments) t
+GROUP BY customer_segment
+ORDER BY total_customers DESC;
---
+++ scripts/EDA/Custumer_Report.sql
@@ -0,0 +1,24 @@
+/*
+----------------------------
+-- Customer Report
+----------------------------
+Purpose: 
+- This report consolidates key customer metrics and behaviors.
+
+Highlights:
+1. Gather essential fields such as names, ages, and transactional details
+2. Segments customers into categories (VIP, Regular, New) and age groups.
+3. Aggregates customer-level metrics:
+    - Total orders
+    - Total spending
+    - Total quantity purchased
+    - Total products purchased
+    - lifespan (in months)
+4. Calculate valuable KPIs:
+    - Recency: Days since the last order
+    - Average order value
+    - Average monthly spending
+
+*/
+
+
